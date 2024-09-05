2d95990ec1066e625a689d6652d4f819a8f2f219
Amy <amy@amogus.cloud>
2024-09-05 17:47:00 -0400

    bump: Initial open source release
    Signed-off-by: Amy <amy@amogus.cloud>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,60 @@
 () => {
-  // This is your plugin's entrypoint, code you type here will be ran as soon as the client starts up
-  // regardless of whether the user's session started.
+	const arrpcSocket = new WebSocket("ws://127.0.0.1:1337")
+	let currentData;
 
-  console.log("Heyo! I am a sample plugin!");
+	arrpcSocket.onmessage = async (x) => {
+		currentData = JSON.parse(x.data);
+	}
+
+	async function fetchAppInfo(appId) {
+		const response = await fetch(`https://discord.com/api/v10/applications/${appId}/rpc`)
+
+		const data = await response.json()
+
+		return data
+	}
+
+	let client;
+	let intrvl;
+	let starting;
+	async function main() {
+		client = await new Promise((res) => {
+			setInterval(() => {
+				if (window.controllers.client.isReady()) res(window.controllers.client.getReadyClient())
+			}, 10)
+		});
+
+		starting = client.user.status.text
+
+		intrvl = setInterval(async () => {
+			if (currentData && currentData.activity && currentData.activity.application_id) {
+				const appinfo = await fetchAppInfo(currentData.activity.application_id);
+				await client.api.patch("/users/@me", {
+					status: {
+						text: `(${appinfo.name}) ${currentData.activity.details ?? ""} ${currentData.activity.state ?? ""}`
+					}
+
+				})
+			} else {
+				if (client.user.status.text !== starting) {
+					await client.api.patch("/users/@me", {
+						status: {
+							text: starting
+						}
+
+					})
+				}
+			}
+		}, 10000)
+	}
+
+	main()
+
+	return {
+		onUnload: async () => {
+			await client.api.patch("/users/@me", { status: { text: starting } });
+			clearInterval(intrvl)
+			arrpcSocket.close(1000, "Plugin unloaded.")
+		}
+	}
 }
